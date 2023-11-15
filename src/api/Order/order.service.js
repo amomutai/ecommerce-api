8a6c1d2738db987b1bdb340d2ba559e3a556ab87
Amos Mutai <amosmutai04@gmail.com>
2023-11-15 22:53:10 +0300

    This commit adds the delete order endpoint

--- src/api/Order/order.service.js
+++ src/api/Order/order.service.js
@@ -3,7 +3,7 @@ const Errors = require("../../errors")
 const prisma = require("../../utils/prisma.util")
 
 const UserNotFound = new Errors.NotFoundError("User does not exists")
-const CartNotFound = new Errors.NotFoundError("Cart does not exists")
+const OrderNotFound = new Errors.NotFoundError("Order does not exists")
 
 class OrderService {
     static async add(data){
@@ -15,6 +15,25 @@ class OrderService {
         const res = await prisma.orders.create({ data: { user_id }})
 
         return res
+    }
+
+    static async delete(id){
+        //Check if category exists first
+        const order = await this.findById(id)
+        if(!order) throw OrderNotFound
+
+        const res = await prisma.orders.delete({ where: { id }})
+
+        return res
+    }
+
+    static async findById(id){
+        const order = await prisma.orders.findUnique({
+            where: { id },
+            select: { id: true }
+        })
+
+        return order
     }
 }
 
